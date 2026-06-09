b9966eb302e742e7d63664b75bb9e5cf3cd419dc
smsmm4269-web <smsmm4269@gmail.com>
2026-06-10 02:38:54 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function sign(){
+    let input= document.getElementById("input").value;
+
+    if(input==12345){
+        window.location.href="SMSM.html";
+    }
+    else{
+        alert("PASSWORD UNCORRECT")
+    }
+}
